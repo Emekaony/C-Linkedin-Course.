e033aebe63cb28b43f5bc8d7de90f108d507ce18
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-12-13 11:33:06 -0800

    Fixed main entry point and code works now.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿using basics.audio;
+﻿namespace Program {
+    public class Basics() {
 
-namespace basics {
-    public class Basics {
-        public static void Main() {
-            AudioTranscriber.Elevate("emeka", 1, 2);
-            Console.WriteLine("hello");
+        public static void Main(string[] args) {
+            int[] numbers = [1, 2, 3];
+            foreach (int number in numbers) {
+                Console.WriteLine($"The number we're looking at is: {number}");
+            }
+            string messageToThePeople = "I will win";
+            if (messageToThePeople.Length > 10) {
+                Console.WriteLine("It is time!");
+            } else {
+                Console.WriteLine("It is still time my brother");
+            }
         }
+
     }
 }
