c7912c9f5097ee8c9105e92ffa15089faae7dd85
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-12-14 10:49:07 -0800

    Changed function argument to match exercise

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace basics {
     public class Basics() {
 
         public static void Main() {
-            List<string> words = ["Race car!", "Madam", "Madam, I'm Adam"];
+            List<string> words = ["Race car!", "Madam", "Madam, I'm Adam", "speak"];
             foreach (string word in words) {
                 Console.WriteLine(TextAlgorithms.IsPalindrome(word));
             }
