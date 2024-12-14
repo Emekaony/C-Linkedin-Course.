0ed8e5c52977bf88d9044a79e37161dbb34a864a
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-12-14 10:47:41 -0800

    Fixed bug in palindrome detectino code using continue

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
-﻿namespace basics {
+﻿using basics.exercises;
+
+namespace basics {
     public class Basics() {
 
-        public static void Main(string[] args) {
-            int[] numbers = [1, 2, 3];
-            foreach (int number in numbers) {
-                Console.WriteLine($"The number we're looking at is: {number}");
-            }
-            string messageToThePeople = "I will win";
-            if (messageToThePeople.Length > 10) {
-                Console.WriteLine("It is time!");
-            } else {
-                Console.WriteLine("It is still time my brother");
+        public static void Main() {
+            List<string> words = ["Race car!", "Madam", "Madam, I'm Adam"];
+            foreach (string word in words) {
+                Console.WriteLine(TextAlgorithms.IsPalindrome(word));
             }
         }
 
