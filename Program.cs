cb3552415211cd7396dc25278d921167d525e757
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-12-21 22:49:01 -0800

    Used default constructor

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
-﻿using basics.exercises;
+﻿// using basics.exercises;
+using basics.oop;
 
 namespace basics {
     public class Basics() {
 
         public static void Main() {
-            List<string> words = ["Race car!", "Madam", "Madam, I'm Adam", "speak"];
-            foreach (string word in words) {
-                Console.WriteLine(TextAlgorithms.IsPalindrome(word));
-            }
+            // List<string> words = ["Race car!", "Madam", "Madam, I'm Adam", "speak"];
+            // foreach (string word in words) {
+            //     Console.WriteLine(TextAlgorithms.IsPalindrome(word));
+            // }
+            Book b1 = new("War and Peace", "Leo Tolstoy", 800);
+            Book b2 = new("Things fall apart", "Chinua Achebe", 464);
+
+            // call methods on each book object
+            b1.GetDescription();
+            b2.GetDescription();
         }
 
     }
